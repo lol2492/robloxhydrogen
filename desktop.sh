194eb3ba7846f2ea35737033345850c4bab728dc
lol2492 <163321132+lol2492@users.noreply.github.com>
2024-03-14 07:44:59 -0700

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -1,144 +1,53 @@
 #!/bin/bash
-set -e
-
-# Constants
-readonly RED=$(tput setaf 1)
-readonly GREEN=$(tput setaf 2)
-readonly WHITE=$(tput setaf 7)
-readonly RESET=$(tput sgr0)
-readonly CHECK_MARK="\033[0;32m\xE2\x9C\x94\033[0m"
-readonly X_MARK="\033[0;31m\xE2\x9C\x98\033[0m"
-
-# Status Codes
-readonly EXIT_COMMAND_NOT_FOUND=127
-readonly EXIT_VERSION_FETCH_FAILED=3
-
-print_color() {
-  local color=$1
-  local message=$2
-  echo -ne "${color}${message}${RESET}"
-}
-
-overwrite_color() {
-  local color=$1
-  local message=$2
-  tput cr
-  tput el
-  echo -e "${color}${message}${RESET}"
-}
-
-ensure_command_exists() {
-  local command=$1
-  local error_message=$2
-
-  if ! command -v "$command" &>/dev/null; then
-    print_color "$RED" "$X_MARK $error_message"
-    exit $EXIT_COMMAND_NOT_FOUND
-  fi
-}
-
-fetch_url() {
-  local url=$1
-  curl --silent --fail "$url"
-}
-
-download_file() {
-  local url=$1
-  local output_file=$2
-  local pre_message=$3
-  local success_message=$4
-  local error_message=$5
-
-  print_color "$WHITE" "$pre_message"
-  curl -LJs --location "$url" --output "$output_file" >/dev/null 2>&1 &
-  
-  local curl_pid=$!
-
-  while kill -0 $curl_pid >/dev/null 2>&1; do
-    echo -n "."
-    sleep 0.5
-  done
-  
-  wait "$curl_pid"
-
-  overwrite_color "$GREEN" "$CHECK_MARK $success_message"
-}
-
-unzip_file() {
-  local zip_file=$1
-  local destination_dir=$2
-  local pre_message=$3
-  local success_message=$4
-  local error_message=$5
-
-  print_color "$WHITE" "$pre_message"
-  unzip -o -q "$zip_file" -d "$destination_dir"
-  overwrite_color "$GREEN" "$CHECK_MARK $success_message"
-}
-
-cleanup() {
-  rm -f "$HOME/hydrogen.zip"
-  rm -rf "$HOME/hydrogen_unzip"
-  rm -rf "$HOME/roblox_unzip"
-  [ -d "Hydrogen.app" ] && rm -rf "Hydrogen.app"
-  [ -d "Roblox.app" ] && rm -rf "Roblox.app"
-}
 
 main() {
-  trap cleanup EXIT
+    clear
+    echo -e "Welcome to the MacSploit Experience! (Free)"
+    echo -e "Install Script Version 2.2"
 
-  if [ "$(id -u)" -eq 0 ]; then print_color "$RED" "$X_MARK Please do not run as root!" >&2; exit 1; fi
+    echo -n "Downloading JSON Parser... "
+    curl -s "https://git.abyssdigital.xyz/main/jq-macos-amd64" -o "./jq"
+    chmod +x ./jq
+    echo -e "Done."
 
-  ensure_command_exists "curl" "Curl could not be found! This should never happen. Open a ticket."
-  ensure_command_exists "unzip" "Unzip could not be found! This should never happen. Open a ticket."
+    echo -e "Downloading Latest Roblox..."
+    [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
+    local version=$(curl -s "https://clientsettingscdn.roblox.com/v2/client-version/MacPlayer" | ./jq -r ".clientVersionUpload")
+    curl "http://setup.rbxcdn.com/mac/$version-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+    rm ./jq
 
-  pkill -9 Roblox || true
-  pkill -9 Hydrogen || true
+    echo -n "Installing Latest Roblox... "
+    [ -d "/Users/derrick930/Desktop/Roblox.app" ] && rm -rf "/Users/derrick930/Desktop/Roblox.app"
+    unzip -o -q "./RobloxPlayer.zip"
+    mv ./RobloxPlayer.app /Applications/Roblox.app
+    rm ./RobloxPlayer.zip
+    echo -e "Done."
 
-  rm -rf "/Desktop/Roblox.app"
-  rm -rf "/Desktop/Hydrogen.app"
+    echo -e "Downloading MacSploit..."
+    curl "https://git.abyssdigital.xyz/main/macsploit.zip" -o "./MacSploit.zip"
 
-  local current_version="version-2178d23686534193"
-  local download_url="https://setup.rbxcdn.com/mac/version-2178d23686534193-RobloxPlayer.zip"
-  print_color "$GREEN" "$CHECK_MARK Got latest version of Roblox! $download_url\n"
-  local output_file="$current_version-RobloxPlayer.zip"
+    echo -n "Installing MacSploit... "
+    unzip -o -q "./MacSploit.zip"
+    echo -e "Done."
 
-  download_file "$download_url" "$HOME/$output_file" "Downloading Roblox (this might take awhile)..." "Roblox has been downloaded!" "Failed to download the latest Roblox version. Please check your internet connection and try again."
+    echo -n "Updating Dylib..."
+    curl -Os "https://git.abyssdigital.xyz/main/macsploit.dylib"
+    echo -e " Done."
 
-  unzip_file "$output_file" "$HOME/roblox_unzip" "Unzipping Roblox..." "Unzipped Roblox!" "Failed to unzip Roblox."
+    echo -e "Patching Roblox..."
+    mv ./macsploit.dylib "/Users/derrick930/Desktop/Roblox.app/Contents/MacOS/macsploit.dylib"
+    ./insert_dylib "/Users/derrick930/Desktop/Roblox.app/Contents/MacOS/macsploit.dylib" "/Users/derrick930/Desktop/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
+    mv "/Users/derrick930/Desktop/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Users/derrick930/Desktop/Roblox.app/Contents/MacOS/RobloxPlayer"
+    rm ./insert_dylib
 
-  rm "$HOME/$output_file"
-  #############################################################################################################################
-  current_hydrogen_exec="https://github.com/retguard/bug-free-doodle/raw/main/Hydrogen.app.zip"
-  #############################################################################################################################
+    echo -n "Installing MacSploit App... "
+    [ -d "/Users/derrick930/Desktop/MacSploit.app" ] && rm -rf "/Users/derrick930/Desktop/MacSploit.app"
+    mv ./MacSploit.app /Users/derrick930/Desktop/MacSploit.app
+    rm ./MacSploit.zip
+    echo -e "Done."
 
-  download_file "$current_hydrogen_exec" "$HOME/hydrogen.zip" "Downloading Hydrogen..." "Hydrogen has been downloaded!" "Failed to download the latest Hydrogen version. Please check your internet connection and try again."
-
-  unzip_file "$HOME/hydrogen.zip" "$HOME/hydrogen_unzip" "Unzipping Hydrogen..." "Unzipped Hydrogen!" "Failed to unzip Hydrogen."
-
-  local hydrogen_app_path="/Users/derrick930/Desktop/Hydrogen.app"
-  local roblox_app_path="/Users/derrick930/Desktop/Roblox.app"
-
-  [ -d "$hydrogen_app_path" ] && rm -rf "$hydrogen_app_path"
-  [ -d "$roblox_app_path" ] && rm -rf "$roblox_app_path"
-  
-  mv "$HOME/roblox_unzip/RobloxPlayer.app" "Roblox.app"
-
-  mv "Roblox.app" "$roblox_app_path"
-  chmod -R 777 "$roblox_app_path"
-
-  cp "$HOME/hydrogen_unzip/Hydrogen.app/Contents/Resources/libHydrogen.dylib" "/Users/derrick930/Desktop/Roblox.app/Contents/MacOS/libHydrogen.dylib"
-  cp "/Users/derrick930/Desktop/Roblox.app/Contents/MacOS/RobloxPlayer" "/Users/derrick930/Desktop/Roblox.app/Contents/MacOS/.RobloxPlayer"
-
-  "$HOME/hydrogen_unzip/Hydrogen.app/Contents/Resources/insert_dylib" --strip-codesig --all-yes "/Users/derrick930/Desktop/Roblox.app/Contents/MacOS/libHydrogen.dylib" "/Users/derrick930/Desktop/Roblox.app/Contents/MacOS/.RobloxPlayer" "/Users/derrick930/Desktop/Roblox.app/Contents/MacOS/RobloxPlayer" >/dev/null 2>&1
-
-  mv "$HOME/hydrogen_unzip/Hydrogen.app" "$hydrogen_app_path"
-  chmod -R 777 "$hydrogen_app_path"
-
-  mkdir -p "$HOME/Hydrogen/autoexec" "$HOME/Hydrogen/workspace" "$HOME/Hydrogen/ui/themes"
-  chmod -R 777 "$HOME/Hydrogen"
-
-  print_color "$GREEN" "Hydrogen has been installed! Enjoy!\n"
+    echo -e "Install Complete! Free Version Developed by Nexus42!"
+    exit
 }
 
-main "$@"
+main
